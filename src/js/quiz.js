b55f7a4e398442083f6cfbefcffa11be79f4efbd
timbersaw4422 <timbersaw442211@gmail.com>
2021-05-14 22:59:36 +0300

    сверстан квиз

---
+++ src/js/quiz.js
@@ -0,0 +1,15 @@
+import CustomSelect from 'vanilla-js-dropdown'
+
+export const quiz = () => {
+
+  const typeSelect = new CustomSelect({
+    elem: 'type-select'
+  })
+
+  const materialSelect = new CustomSelect({
+    elem: 'meterial-select'
+  })
+
+  const quizBody = document.querySelector('.quiz-body')
+
+}
--- src/quiz.js
+++ src/quiz.js
@@ -2,3 +2,7 @@ import './scss/common.scss'
 import './scss/header.scss'
 import './scss/footer.scss'
 import './scss/quiz.scss'
+
+import { quiz } from './js/quiz'
+
+quiz()
